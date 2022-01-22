a88dee54f145683d6a9ecc442c4c1b98aee90244
eldomador <johncenajakub@gmail.com>
2022-01-22 21:42:44 +0100

    refactor: I used the formula for artmty strings

--- Sum-of-numbers/getSum.js
+++ Sum-of-numbers/getSum.js
@@ -1,13 +1,5 @@
 const getSum = (a, b) => {
-  sum = 0;
-  if (a <= b) {
-    for (let i = a; i <= b; i++) {
-      sum += i;
-    }
-  } else if (a >= b) {
-    for (let i = b; i <= a; i++) {
-      sum += i;
-    }
-  }
-  return sum;
+  let min = Math.min(a, b);
+  let max = Math.max(a, b);
+  return ((max - min + 1) * (min + max)) / 2;
 };
