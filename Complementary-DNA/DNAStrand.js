81edebd3555bfe7e56c9cd92d48e36f4335d8816
eldomador <johncenajakub@gmail.com>
2022-01-23 15:15:21 +0100

    feat: adding complementary-DNA

---
+++ Complementary-DNA/DNAStrand.js
@@ -0,0 +1,17 @@
+const DNAStrand = (dna) => {
+  const arr = dna.toUpperCase().split("");
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == "A") {
+      arr[i] = "T";
+    } else if (arr[i] == "T") {
+      arr[i] = "A";
+    } else if (arr[i] == "C") {
+      arr[i] = "G";
+    } else if (arr[i] == "G") {
+      arr[i] = "C";
+    }
+  }
+
+  return arr.join("");
+};
